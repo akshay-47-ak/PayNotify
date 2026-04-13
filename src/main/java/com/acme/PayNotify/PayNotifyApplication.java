77d8c59ac5c8725ad850cc041bf919efdf2bf5d0
akshay-47-ak <akshayathavale1525@gmail.com>
2026-04-13 11:49:56 +0530

    Projet Creation and Setup is Completed

---
+++ src/main/java/com/acme/PayNotify/PayNotifyApplication.java
@@ -0,0 +1,13 @@
+package com.acme.PayNotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayNotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayNotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/PayNotify/PayNotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.PayNotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayNotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
